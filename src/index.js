75036b6636f0860bf02986044ba71575398fba63
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-10-28 16:31:56 +0100

    i just add some comments to better understand all the functions in this code

--- src/index.js
+++ src/index.js
@@ -6,20 +6,25 @@ let regionsList = require("../json/region.json");
 
 const { writeJson } = require("./writeJson.js");
 
-
+// create a function to get the each city and add it to the city object
 const city = citiesList.map(function (item) {
   return { id: item.id, name: item.ville, label: item.ville };
 });
 
+// create a function to get the each region and add it to the region object
 const region = regionsList.map(function (item) {
   return { id: item.id, name: item.region, region: item.region };
 });
 
+// create a function to get the each city with its region and add it to the cityWithRegion object
 const regionWithCities = citiesList.map(function (item) {
     const region = regionsList.filter((reg) => reg.id == item.region)[0].region;
     return { id: item.id, name: item.ville, label: item.ville, region: region }; 
 });
 
-writeJson("cities_with_region", regionWithCities);
+// write the city object to a json file with the name city and save it in the data folder
 writeJson("city", city);
+// write the region object to a json file with the name region and save it in the data folder
 writeJson("region", region);
+// write the cityWithRegion object to a json file with the name cities_with_region and save it in the data folder
+writeJson("cities_with_region", regionWithCities);
--- src/writeJson.js
+++ src/writeJson.js
@@ -11,6 +11,11 @@ const write = require("write-json");
  *
  */
 
+/**
+ * create a function to write a json file and save it in another folder with
+ * two variables (fileName & data) fileName is the name of the file and data is
+ * the data to be written in the file and exported to use it in index.js file
+ */
 function writeJson(fileName, data) {
   write(`./data/${fileName}.json`, data, (err) => {
     if (err) {
